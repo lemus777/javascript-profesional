7d20858d0a771cf3f899c30efdbb3481cbab1a5a
Lemus <lemus777@gmail.com>
2022-08-14 13:22:34 +0200

    cambios para getter y setter

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,10 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] }); // creamos nueva instancia de nuestro MediaPlayer llamado player y declaramos que el va a ser video, que está declarado en html, por eso lo pillamos al inicio con document.querySelector("video")
+const player = new MediaPlayer({ 
+    el: video, 
+    plugins: [new AutoPlay()] 
+}); // creamos nueva instancia de nuestro MediaPlayer llamado player y declaramos que el va a ser video, que está declarado en html, por eso lo pillamos al inicio con document.querySelector("video")
 
 const play = document.querySelector(".play-pause");
 play.onclick = () => player.togglePlay();
