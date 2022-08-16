59025bfe1ac5e168c219fb8eb042d50b95c5caed
Lemus <lemus777@gmail.com>
2022-08-16 13:17:00 +0200

    plugin ads y fin del curso

--- assets/index.ts
+++ assets/index.ts
@@ -1,17 +1,18 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay(), new AutoPause()],
+  plugins: [new AutoPlay(), new AutoPause(), new Ads()],
 });
 
-const playButton: HTMLElement = document.querySelector('#playButton');
+const playButton: HTMLElement = document.querySelector('#playButton')!;
 playButton.onclick = () => player.togglePlay();
 
-const muteButton: HTMLElement = document.querySelector('#muteButton');
+const muteButton: HTMLElement = document.querySelector('#muteButton')!;
 muteButton.onclick = () => {
   if (player.media.muted) {
     player.unmute();
